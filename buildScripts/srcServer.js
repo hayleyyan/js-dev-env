5b4169e50f1bb5b38bc70e2a7665b17c61c0d16b
hayleyYan <hayley.Yan@target.com>
2018-11-19 15:14:14 -0600

    add eslint json file

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,6 +4,7 @@ import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
+/* eslint-disable no-console */
 const port = 5000;
 const app = express();
 const complier = webpack(config);
